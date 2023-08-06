af0c60aa19f084ee36696838be35711679962660
Queensley <102861927+QueensleyC@users.noreply.github.com>
2023-08-06 20:48:24 +0100

    Update student_class.py

--- student_class.py
+++ student_class.py
@@ -1,9 +1,10 @@
 class Student():
   CGPA = 0.0
-  def __init__(self, name, age, reg_no):
+  def __init__(self, name, age, reg_no, ethnicity):
     self.name = name,
     self.age = age,
-    self.reg_no = reg_no
+    self.reg_no = reg_no,
+    self.ethnicity = ethnicity
 
   def cgpa(self):
     self.cgpa = CGPA
